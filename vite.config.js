c7ff668191495a4f53988e879e9354d2e3e7d624
Devz-0047 <devendersingh00747@gmail.com>
2024-12-13 22:46:42 +0530

    C V: Building Project

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
+  // Base URL for GitHub Pages deployment
+  base: "/HealthFirmLandingPage/",
+
+  // Tailwind CSS Configuration
   css: {
     postcss: {
       plugins: [tailwindcss],
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: "index.html",
     },
   },
 });
